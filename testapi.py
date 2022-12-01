b6b1b90ce324082904b9d46a0fc2679956159239
szaqadir <54589305+szaqadir@users.noreply.github.com>
2022-11-30 22:18:05 -0500

    added some code

--- testapi.py
+++ testapi.py
@@ -2,6 +2,8 @@ from bottle import route, run, request
 import spotipy
 from spotipy import oauth2
 import matplotlib.pyplot as plt
+from collections import ChainMap
+
 
 PORT_NUMBER = 8080
 SPOTIPY_CLIENT_ID = 'e43d44dd0cc34a91a559ba1c872bb2b8'
@@ -41,22 +43,32 @@ def index():
         artist_name = []
         track_id = []
         songs = sp.current_user_top_tracks(limit=50,time_range='long_term')
+        songs2 = sp.current_user_top_tracks(offset=50, limit=50,time_range='long_term')
+        songs3 = sp.current_user_top_tracks(offset=100, limit=50,time_range='long_term')
+        songs4 = sp.current_user_top_tracks(offset=150, limit=50,time_range='long_term')
+        # song = ChainMap(songs, songs2)
+        # song2 = ChainMap(songs3, songs4)
+        # finalSong = ChainMap(song, song2)
+        # print(finalSong)
+
         for i, item in enumerate(songs['items']):
             track_name.append(item['name'])
             s = item['album']['release_date']
             release_date.append(int(s[0:4]))
             artist_name.append(item['artists'][0]['name'])
             track_id.append(item['id'])
-
         # loading lists into the dataframe
-        df = pd.DataFrame({'track_name':track_name, 'release_date':release_date})
-        md = df.groupby('release_date').count().to_dict(orient='dict')['track_name']
-        print(md)
 
-        #TODO: sort by release date, make a line plot
+        df = pd.DataFrame({'track_name':track_name, 'release_date':release_date})
+
+
+
+        md = df.groupby('release_date').count().to_dict(orient='dict')['track_name']
         dates = list(md.keys())           # list() needed for python 3.x
-        songs = list(md.values())        # ditto
-        plt.plot(dates, songs, '-') # this will show date at the x-axis
+        numSongs = list(md.values())        # ditto
+        plt.plot(dates, numSongs, '-') # this will show date at the x-axis
+        plt.xlabel("Year of Song Release")
+        plt.ylabel("Number of Songs Released")
         # df.plot()
         plt.show()
 
